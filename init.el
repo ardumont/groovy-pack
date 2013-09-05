492e2a6f0374bca78adfdfd32a3dcc1d0ad90a45
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-05 13:54:43 +0200

    pack http://groovy.codehaus.org/Emacs+Groovy+Mode

--- init.el
+++ init.el
@@ -1,7 +1,18 @@
-;; User pack init file
-;;
-;; Use this file to initiate the pack configuration.
-;; See README for more information.
+(install-pack 'groovy-mode)
+
+;;; turn on syntax highlighting
+(global-font-lock-mode 1)
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
